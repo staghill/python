8f38ab5a0d976b2487c0395c4f8a6969c6a70f4c
staghill <benjamin.zhang@gmail.com>
2014-04-04 15:03:50 +0800

    the practice inn coursera python
    the Quiz 1, question 6

---
+++ Python/coursera_quiz1.py
@@ -0,0 +1,8 @@
+def future_value(present_value, annual_rate, periods_per_year, years):
+    rate_per_period = annual_rate / periods_per_year
+    periods = periods_per_year * years
+    
+    # Put your code here.
+    return (1 + rate_per_period) ** periods * present_value
+
+print "$1000 at 2% compounded daily for 3 years yields $", future_value(1000, .02, 365, 3)
